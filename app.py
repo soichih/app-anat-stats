a4ba812c266dbbfee51b48bcc84476284a1cb923
Soichi Hayashi <soichih@gmail.com>
2022-04-04 15:07:21 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import nibabel
+import numpy as np
+import io
+
+with open('config.json', encoding='utf8') as config_json:
+    config = json.load(config_json)
+
+img = nibabel.load(config["t1"])
+
+results = {}
+
+results["headers"] = {}
+for key in img.header:
+    value = img.header[key]
+    results['headers'][key] = value
+results['headers']['base_affine'] = img.header.get_base_affine().tolist()
+
+data = img.get_fdata()
+results["voxel_histogram"] = np.histogram(data, bins=100, range=(0, 3000))
+
+if not os.path.exists("output"):
+    os.makedirs("output")
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, bytes):
+            return obj.decode("utf-8")
+        return json.JSONEncoder.default(self, obj)
+
+with open("output/stats.json", "w") as outfile:
+    json.dump(results, outfile, cls=NumpyEncoder)
+
+print("done");
